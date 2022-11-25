575d76e43d0476ae6df8ec95ec79379cdb07a611
CesarNr <cesar.inuncira@gmail.com>
2022-11-25 18:17:49 -0500

    Commit Test 2511221

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Super Tiernos</a>
       </header>
-      <main>Lista de productos</main>
+      <main>
+        <h1> Lista de productos </h1>
+        <div className="products">
+            {
+              data.products.map( (product) => (
+                <div className="product" key={product._id}>
+                  <a href={`/product/${product._id}`}>
+                    <img src={product.image} alt={product.name}/>
+                  </a>
+                  <div className="product-info">
+                  <a href={`/product/${product._id}`}>
+                      <p>{product.name}</p>
+                    </a>
+                    <p><strong>${product.price}</strong></p>
+                    <button>Agregar al carrito</button>
+                  </div>
+                </div>
+              ))
+            }
+        </div>
+      </main>
     </div>
   );
 }
