bc733dd187e37acc281237d54c78438d45396ac2
CesarNr <cesar.inuncira@gmail.com>
2023-05-11 23:43:16 -0500

    Test1_Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,39 @@
 import data from "./data";
+import { BroswerRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Super Tiernos</a>
-      </header>
-      <main>
-        <h1> Lista de productos </h1>
-        <div className="products">
-            {
-              data.products.map( (product) => (
-                <div className="product" key={product._id}>
-                  <a href={`/product/${product._id}`}>
-                    <img src={product.image} alt={product.name}/>
-                  </a>
-                  <div className="product-info">
-                  <a href={`/product/${product._id}`}>
-                      <p>{product.name}</p>
+    <Router>
+      <div>
+        <header>
+          <a href="/">Super Tiernos</a>
+        </header>
+        <main>
+          <Routes>
+            <Route Path="/" element={<HomeScreen/>}/>
+          </Routes>
+          <h1> Lista de productos </h1>
+          <div className="products">
+              {
+                data.products.map( (product) => (
+                  <div className="product" key={product._id}>
+                    <a href={`/product/${product._id}`}>
+                      <img src={product.image} alt={product.name}/>
                     </a>
-                    <p><strong>${product.price}</strong></p>
-                    <button>Agregar al carrito</button>
+                    <div className="product-info">
+                    <a href={`/product/${product._id}`}>
+                        <p>{product.name}</p>
+                      </a>
+                      <p><strong>${product.price}</strong></p>
+                      <button>Agregar al carrito</button>
+                    </div>
                   </div>
-                </div>
-              ))
-            }
-        </div>
-      </main>
-    </div>
+                ))
+              }
+          </div>
+        </main>
+      </div>
+    </Router>
   );
 }
 
